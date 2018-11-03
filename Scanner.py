c1b228035aa4f9f311ffe08bcc196a4329ff024b
Curtis Poe <curtis2point0@gmail.com>
2018-11-03 11:01:06 -0500

    Separtated testing into own file.

--- Scanner.py
+++ Scanner.py
@@ -75,20 +75,3 @@ class Scanner:
 class ScannerError(Exception):
     """Raised when Scanner encounters a possible error to be handled accordingly"""
     pass
-
-
-
-# TESTING
-testScan = Scanner('192.168.1.1', '7-1024')
-try:
-    all_hosts = testScan.get_hosts()
-except ScannerError as err:
-    print(err)
-
-testScan.fast_scan()
-print(testScan.get_hosts())
-testScan.print_scan()
-
-testScan.detect_os_service_scan()
-testScan.print_scan()
-
