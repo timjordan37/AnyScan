dec01ab95f4a6536404101d3db1396681d4ba653
Shayne Torres <shayneptorres@gmail.com>
2019-01-29 18:57:05 -0800

    Task115 - build simple prototype so sample note book gui

---
+++ python_gui/tkinter_tabs.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import ttk
+
+print("Tabs Prototype started...")
+
+# Setup root ui
+root = tk.Tk()
+root.title("Tabs Prototype")
+
+rows = 0
+
+while rows < 50:
+    root.rowconfigure(rows, weight=1)
+    root.columnconfigure(rows, weight=1)
+    rows += 1
+
+# Setup Root Notebook
+main_note_book = ttk.Notebook(root)
+main_note_book.grid(row=1, column=0, columnspan=50, rowspan=49, sticky="NESW")
+
+# Setup Tab 1
+tab1 = tk.Frame(main_note_book)
+main_note_book.add(tab1, text="Tab 1")
+
+# Setup Tab 1
+tab2 = tk.Frame(main_note_book)
+main_note_book.add(tab2, text="Tab 2")
+
+# Run the program with UI
+root.geometry("800x500")
+root.minsize(800, 500)
+root.mainloop()
